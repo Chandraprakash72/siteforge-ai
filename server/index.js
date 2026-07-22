29535287d48550666b0c713e55523a78c7c77958
Chandra prakash maurya <hsv7238@gmail.com>
2026-07-22 17:55:14 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const port=process.env.PORT||5000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://siteforge-ai-1-hvxj.onrender.com",
     credentials:true
 }))
 app.use("/api/auth",authRouter)
